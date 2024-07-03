e19acddc2879ae6964ade280696933911a3f2045
Vidhya Satpute <vidya.satpute@pinnacleway.work>
2024-07-03 15:19:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+
+
+const port = process.env.PORT;
+
+app.get('/', (req, res)=>{
+    res.send('slash route is called')
+})
+
+app.get('/twitter', (req, res)=>{
+    res.send('twitter route is called')
+})
+
+app.get('/login', (req, res)=>{
+    res.send(`<h1>Please login </h1>`)
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app is listening to the port ${port}`);
+})
